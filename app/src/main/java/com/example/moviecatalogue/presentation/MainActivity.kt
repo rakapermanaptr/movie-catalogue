b7601fd44b984723f2c67e27064d11c22bd84038
rakapermanaptr <raka.putra@cxrus.com>
2020-04-25 15:09:08 +0700

    setup sections viewpager on main activity

--- app/src/main/java/com/example/moviecatalogue/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalogue/presentation/MainActivity.kt
@@ -3,11 +3,25 @@ package com.example.moviecatalogue.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moviecatalogue.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupView()
+
+    }
+
+    private fun setupView() {
+        // setup viewpager for sections
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        view_pager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(view_pager)
+
+        // remove shadow on toolbar
+        supportActionBar?.elevation = 0f
     }
 }
